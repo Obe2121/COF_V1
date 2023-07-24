e8d786ee9233917f75675e53db5b620658574ea9
Obe2121 <Obe2121@gmail.com>
2023-07-24 16:53:20 -0400

    Initial Commit

---
+++ COF/COFApp.swift
@@ -0,0 +1,17 @@
+//
+//  COFApp.swift
+//  COF
+//
+//  Created by Mike Oberdick on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct COFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
